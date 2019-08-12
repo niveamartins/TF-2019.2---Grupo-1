e2fa6c9e7dd9137ed3fcc6fe7d6cd6a6df4389c4
Nivea <niveamartins@dcc.ufrj.br>
2019-08-12 02:06:11 -0300

    +Criação da Página dos Posts

---
+++ front-end/src/app/post/post.page.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.page.html',
+  styleUrls: ['./post.page.scss'],
+})
+export class PostPage implements OnInit {
+  ImagemPost = '';
+  TituloPost = '';
+  TextoPost = '';
+
+  //public postService: PostService
+  constructor( ) { }
+
+  //getPost():void {
+  //  this.postService.getPost().subscribe(
+  //    (res) => {
+  //      console.log(res);
+  //    }
+  //  )
+  //}
+  ngOnInit() {
+    //this.getPost();
+  }
+
+}
