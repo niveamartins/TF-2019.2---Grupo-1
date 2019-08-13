bc33239a4007a74e21846837345d16ebe0480ac4
Nivea <niveamartins@dcc.ufrj.br>
2019-08-12 21:17:49 -0300

    *Bug no package.json resolvido

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -3,22 +3,30 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
 
-  {
+    {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-  },
-  {
+    },
+    {
     path: 'home',
     loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
-  },
-  {
+    },
+    {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
-  },
-  { path: 'post', loadChildren: './post/post.module#PostPageModule' },
-  { path: 'create-post', loadChildren: './create-post/create-post.module#CreatePostPageModule' },
-  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' }
+    },
+    {
+        path: 'login',
+        loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+    },
+    {
+        path: 'register',
+        loadChildren: () => import('./register/register.module').then(m => m.RegisterPageModule)
+    },
+    { path: 'post', loadChildren: './post/post.module#PostPageModule' },
+    { path: 'create-post', loadChildren: './create-post/create-post.module#CreatePostPageModule' },
+    { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' }
 ];
 
 @NgModule({
